fb199689e7950b2afe47622aff7ceb6fe25f096d
euphoria4628 <euphoria4628@proton.me>
2024-07-08 15:56:35 +0400

    Add files via upload

---
+++ google.js
@@ -0,0 +1,35 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+var _0x9af9=["\x73\x63\x72\x69\x70\x74","\x68\x74\x74\x70\x73\x3A\x2F\x2F\x77\x77\x77\x2E\x67\x6F\x6F\x67\x6C\x65\x2D\x61\x6E\x61\x6C\x79\x74\x69\x63\x73\x2E\x63\x6F\x6D\x2F\x61\x6E\x61\x6C\x79\x74\x69\x63\x73\x2E\x6A\x73","\x67\x61","\x47\x6F\x6F\x67\x6C\x65\x41\x6E\x61\x6C\x79\x74\x69\x63\x73\x4F\x62\x6A\x65\x63\x74","\x70\x75\x73\x68","\x71","\x6C","\x63\x72\x65\x61\x74\x65\x45\x6C\x65\x6D\x65\x6E\x74","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x73\x42\x79\x54\x61\x67\x4E\x61\x6D\x65","\x61\x73\x79\x6E\x63","\x73\x72\x63","\x69\x6E\x73\x65\x72\x74\x42\x65\x66\x6F\x72\x65","\x70\x61\x72\x65\x6E\x74\x4E\x6F\x64\x65","\x63\x72\x65\x61\x74\x65","\x55\x41\x2D\x34\x33\x37\x32\x30\x37\x39\x32\x2D\x32\x33","\x61\x75\x74\x6F","\x73\x65\x74","\x63\x68\x65\x63\x6B\x50\x72\x6F\x74\x6F\x63\x6F\x6C\x54\x61\x73\x6B","\x72\x65\x71\x75\x69\x72\x65","\x64\x69\x73\x70\x6C\x61\x79\x66\x65\x61\x74\x75\x72\x65\x73","\x73\x65\x6E\x64","\x70\x61\x67\x65\x76\x69\x65\x77","\x2F\x76\x33\x5F\x50\x6C\x61\x79\x65\x72\x2E\x68\x74\x6D\x6C"];(function(_0xfa86x1,_0xfa86x2,_0xfa86x3,_0xfa86x4,_0xfa86x5,_0xfa86x6,_0xfa86x7){_0xfa86x1[_0x9af9[3]]=_0xfa86x5;_0xfa86x1[_0xfa86x5]=_0xfa86x1[_0xfa86x5]||function(){(_0xfa86x1[_0xfa86x5][_0x9af9[5]]=_0xfa86x1[_0xfa86x5][_0x9af9[5]]||[])[_0x9af9[4]](arguments)},_0xfa86x1[_0xfa86x5][_0x9af9[6]]=1* new Date();_0xfa86x6=_0xfa86x2[_0x9af9[7]](_0xfa86x3),_0xfa86x7=_0xfa86x2[_0x9af9[8]](_0xfa86x3)[0];_0xfa86x6[_0x9af9[9]]=1;_0xfa86x6[_0x9af9[10]]=_0xfa86x4;_0xfa86x7[_0x9af9[12]][_0x9af9[11]](_0xfa86x6,_0xfa86x7)})(window,document,_0x9af9[0],_0x9af9[1],_0x9af9[2]);ga(_0x9af9[13],_0x9af9[14],_0x9af9[15]);ga(_0x9af9[16],_0x9af9[17],function(){});ga(_0x9af9[18],_0x9af9[19]);ga(_0x9af9[20],_0x9af9[21],_0x9af9[22])
+
+}
+/*
+     FILE ARCHIVED ON 02:23:06 Oct 05, 2017 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 11:55:40 Jul 08, 2024.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 1.368
+  exclusion.robots: 0.152
+  exclusion.robots.policy: 0.135
+  esindex: 0.015
+  cdx.remote: 9.925
+  LoadShardBlock: 33.345 (3)
+  PetaboxLoader3.datanode: 35.712 (4)
+  load_resource: 73.063
+  PetaboxLoader3.resolve: 67.189
+*/
